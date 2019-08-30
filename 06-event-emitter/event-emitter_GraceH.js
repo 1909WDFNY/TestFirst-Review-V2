144ff94dd0b583625264e205472713e1b234255a
1909WDFNY <54694024+1909WDFNY@users.noreply.github.com>
2019-08-30 15:05:36 -0400

    Update event-emitter_GraceH.js

--- 06-event-emitter/event-emitter_GraceH.js
+++ 06-event-emitter/event-emitter_GraceH.js
@@ -5,12 +5,9 @@ class MyEventEmitter {
         this.events = {};
     }
     addListener(funcName, func) {
-        if (!this.events[funcName]) this.events[funcName] = [func];
-        else this.events[funcName].push(func);
+        (!this.events[funcName]) ? this.events[funcName] = [func] : this.events[funcName].push(func);
     }
     emit(callback, ...args) {
-        this.events[callback].forEach(func => {
-            func.apply(null, [...args]);
-        })
+        this.events[callback].forEach(func => func.apply(null, [...args]));
     }
 }
