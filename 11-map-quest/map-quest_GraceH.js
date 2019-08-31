49af325b21f8af2e961b1ad5f0bc244507045c2c
1909WDFNY <54694024+1909WDFNY@users.noreply.github.com>
2019-08-31 18:13:55 -0400

    Create map-quest_GraceH.js

---
+++ 11-map-quest/map-quest_GraceH.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+
+const mapQuest = arr => {
+    let dupPair = {
+        N: 'S',
+        S: 'N',
+        E: 'W',
+        W: 'E'
+    };
+    let resultArr = arr.reduce((acc, currDir) => {
+        (dupPair[acc[acc.length-1]] === currDir) ? acc.pop() : acc.push(currDir);
+        return acc;
+    }, []);
+    return resultArr;
+}
+
+// with an array and recursion:
+
+// const mapQuest = arr => {
+//     let count = 0;
+//     let dupPair = ['NS', 'SN', 'EW', 'WE'];
+//     let resultArr = arr.reduce((acc, currDir, idx, thisArr) => {
+//         if (dupPair.includes(thisArr[idx-1] + currDir)) {
+//             count ++;
+//             acc.pop();
+//         }
+//         else acc.push(currDir);
+//         return acc;
+//     }, []);
+
+//     //keep running mapQuest on itself until there are no more redundant direction pairs
+//     if (count !== 0) return mapQuest(resultArr);
+//     return resultArr;
+// }
