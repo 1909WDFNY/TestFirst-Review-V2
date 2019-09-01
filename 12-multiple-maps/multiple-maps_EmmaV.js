f3789a9f1fb0866488ecc49a581af8a22adb08e2
1909WDFNY <54694024+1909WDFNY@users.noreply.github.com>
2019-09-01 17:42:55 -0400

    refactored with for in loop instead of for loop

--- 12-multiple-maps/multiple-maps_EmmaV.js
+++ 12-multiple-maps/multiple-maps_EmmaV.js
@@ -1,12 +1,12 @@
 const multipleMaps = (keysArray, objArray) => {
-    const finalArray = keysArray.map(function(keyFromArray) {
-        for (let i = 0; i < objArray.length; i++) {
-            let currentObj = objArray[i];
-            if (currentObj.hasOwnProperty(keyFromArray)) {
-                return currentObj[keyFromArray];
+    let finalArray = keysArray.map(function(keyFromArray) {
+        for (const obj of objArray) {
+            if (obj.hasOwnProperty(keyFromArray)) {
+                return obj[keyFromArray];
             }
         }
     });
     return finalArray;
 
 }
+
