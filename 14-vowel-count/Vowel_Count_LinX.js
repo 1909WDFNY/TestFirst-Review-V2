b69b7d09a055669f1e7b0eba1989c998fc5eb2d1
1909WDFNY <54694024+1909WDFNY@users.noreply.github.com>
2019-09-01 22:52:53 -0400

    Create Vowel_Count_LinX.js

---
+++ 14-vowel-count/Vowel_Count_LinX.js
@@ -0,0 +1,18 @@
+function vowelsCount(str) {
+    let fixed = str.toLowerCase();
+    let vowels = "aeiou";
+    let vowelObj = {a: 0, e: 0, i: 0, o: 0, u: 0, total: 0};
+    let charArr = fixed.split("");
+
+    if(str.length === 0) {return vowelObj};
+
+    charArr.reduce((acc, curVal) => {
+        if(vowels.includes(curVal)) {
+            vowelObj[curVal]++;
+            acc++;
+        }
+        return vowelObj.total = acc
+    },0)
+
+    return vowelObj;
+}
